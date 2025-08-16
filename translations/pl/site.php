f091e42f25dbac930a4c8f8089fb20436ab95c39
Piotr Pogorzelski <piotrpog@protonmail.com>
2025-08-16 16:33:54 +0200

    translations

---
+++ translations/pl/site.php
@@ -0,0 +1,73 @@
+<?php
+return [
+    'field.pageBuilder' => 'Bloki',
+    'field.color' => 'Kolor',
+    'field.richText' => 'Edytor tekstowy',
+    'field.blockHash' => 'Hash',
+    'field.singleLink' => 'Link',
+    'field.linksList' => 'Linki',
+    'field.imageAsset' => 'Obraz',
+
+    'field.paddingBottomOverrideDesktop' => 'Odstęp od dołu',
+    'field.paddingTopOverrideDesktop' => 'Odstęp od góry',
+    'field.paddingBottomOverrideMobile' => 'Odstęp od dołu - wersja mobilna',
+    'field.paddingTopOverrideMobile' => 'Odstęp od góry - wersja mobilna',
+
+    'field.textEmphasized' => 'Tekst z wyróżnieniem',
+    'field.textEmphasized.instructions' => 'Użyj pogrubienia by wyróżnić kolorystycznie fragment tekstu.',
+    'field.plainText' => 'Tekst',
+    'field.showDefaultTitleSection' => 'Pokaż domyślny nagłówek',
+
+    'field.openGraphImage' => 'SEO - Obraz Open Graph',
+    'field.seoAltTitle' => 'SEO - Alternatywny tytuł',
+    'field.seoDescription' => 'SEO - Opis',
+    'field.seoRobots' => 'SEO - Instrukcja dla robotów',
+    'field.seoSuffix' => 'SEO - Końcówka tytułu',
+
+    'field.sharedContentEntries' => 'Bloki',
+
+    'field.blockSettings' => 'Ustawienia bloku',
+    'field.number' => 'Numer',
+    'field.distance' => 'Odległość',
+    'field.plainTextSingleLine' => 'Tekst pojedyncza linia',
+    'field.switchDisabled' => 'Przełącznik (wył.)',
+
+    // child fields
+    'field.textHeader' => 'Tekst nagłówka',
+    'field.colorText' => 'Kolor tekstu',
+    'field.colorBackground' => 'Kolor tła',
+
+    // assets
+    'volume.localVolume' => 'Lokalne zasoby',
+    'fs.LocalFs' => 'Lokalny system plików',
+
+    // sections
+    'section.contentPage' => 'Strony',
+    'section.sharedContent' => 'Bloki wielokrotnego użytku',
+
+    // page builder settings
+    'tab.content' => 'Treść',
+    'tab.settings' => 'Ustawienia',
+    'tab.seo' => 'Seo',
+    'field.pageBuilder.add' => 'Dodaj blok',
+
+    // entry types
+    'entryType.contentPage' => 'Strony - pola',
+    'entryType.linksList' => 'Linki - pola',
+    'entryType.sharedContentEntry' => 'Bloki wielokrotnego użytku - wpisy - pola',
+
+    // entry types - blocks
+    'block.header' => 'Nagłówek',
+    'block.blockSharedContent' => 'Bloki wielokrotnego użytku',
+
+    // globals
+    'global.globalSeo' => 'Ustawienia SEO',
+    'global.navTop' => 'Menu główne',
+
+    'matrix.add' => 'Dodaj',
+
+    // templates
+    'header.notFound' => 'Nie znaleziono strony',
+
+
+];
