addfd47f81dd49ab94e3aa5b30fa918a3d693670
Piotr Pogorzelski <piotrpog@protonmail.com>
2025-08-17 19:53:25 +0200

    translations

---
+++ translations/en/site.php
@@ -0,0 +1,77 @@
+<?php
+return [
+    'field.pageBuilder' => 'Blocks',
+    'field.color' => 'Color',
+    'field.richText' => 'Rich Text Editor',
+    'field.blockHash' => 'Hash',
+    'field.singleLink' => 'Link',
+    'field.linksList' => 'Links',
+    'field.imageAsset' => 'Image',
+
+    'field.paddingBottomOverrideDesktop' => 'Bottom Padding',
+    'field.paddingTopOverrideDesktop' => 'Top Padding',
+    'field.paddingBottomOverrideMobile' => 'Bottom Padding - Mobile',
+    'field.paddingTopOverrideMobile' => 'Top Padding - Mobile',
+
+    'field.textEmphasized' => 'Emphasized Text',
+    'field.textEmphasized.instructions' => 'Use bold to highlight part of the text with color.',
+    'field.plainText' => 'Text',
+    'field.showDefaultTitleSection' => 'Show Default Header',
+
+    'field.openGraphImage' => 'SEO - Open Graph Image',
+    'field.seoAltTitle' => 'SEO - Alternative Title',
+    'field.seoDescription' => 'SEO - Description',
+    'field.seoRobots' => 'SEO - Robots Instruction',
+    'field.seoSuffix' => 'SEO - Title Suffix',
+
+    'field.sharedContentEntries' => 'Selected Blocks',
+
+    'field.blockSettings' => 'Block Settings',
+    'field.number' => 'Number',
+    'field.distance' => 'Distance',
+    'field.plainTextSingleLine' => 'Single-line Text',
+    'field.switchDisabled' => 'Switch (off)',
+
+    'field.sectionHeader' => 'Section Header',
+
+    // child fields
+    'field.textHeader' => 'Header Text',
+    'field.colorText' => 'Text Color',
+    'field.colorBackground' => 'Background Color',
+    'field.headerText' => 'Header Text',
+    'field.richContent' => 'Text Content',
+    'field.sectionHeaderMain' => 'Section Header - Main',
+
+    // assets
+    'volume.localVolume' => 'Local Assets',
+    'fs.LocalFs' => 'Local File System',
+
+    // sections
+    'section.contentPage' => 'Pages',
+    'section.sharedContent' => 'Reusable Blocks',
+
+    // page builder settings
+    'tab.content' => 'Content',
+    'tab.settings' => 'Settings',
+    'tab.seo' => 'SEO',
+    'field.pageBuilder.add' => 'Add Block',
+
+    // entry types
+    'entryType.contentPage' => 'Pages - Fields',
+    'entryType.linksList' => 'Links - Fields',
+    'entryType.sharedContentEntry' => 'Reusable Blocks - Entries - Fields',
+
+    // entry types - blocks
+    'block.header' => 'Header',
+    'block.blockSharedContent' => 'Reusable Blocks',
+    'block.richText' => 'Text',
+
+    // globals
+    'global.globalSeo' => 'SEO Settings',
+    'global.navTop' => 'Main Menu',
+
+    'matrix.add' => 'Add',
+
+    // templates
+    'header.notFound' => 'Page Not Found',
+];
